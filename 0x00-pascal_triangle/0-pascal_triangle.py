de667e3a22d636c8b10ea655277b855213910c2a
zaki82000 <yabdelshafy0@gmail.com>
2024-05-31 00:16:01 +0300

    add pascal triangle file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module to draw pascal"""
+
+
+def pascal_triangle(n):
+    '''Creates a list of lists of
+    the Pascal's triangle of n size
+    '''
+    if n <= 0 or type(n) is not int:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == i or j == 0:
+                row.append(1)
+            if i > j and j > 0:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+
+    return triangle
