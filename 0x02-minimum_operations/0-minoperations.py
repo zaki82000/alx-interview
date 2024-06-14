f50fde1848dd329e0751a91c94f743e7bb384906
zaki82000 <yabdelshafy0@gmail.com>
2024-06-14 03:51:19 +0300

    add minoperations python file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''The minimum operations module
+'''
+
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
